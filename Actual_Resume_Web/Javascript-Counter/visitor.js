1fdfa74fecea59149c8be840c4022b7c118348cc
zirui2333 <zhengzirui43@gmail.com>
2024-05-22 19:56:53 -0400

    change visitor.js Function URL

--- Actual_Resume_Web/Javascript-Counter/visitor.js
+++ Actual_Resume_Web/Javascript-Counter/visitor.js
@@ -2,7 +2,7 @@ const counter = document.querySelector(".counter_views")
 
 async function update_views(){
   try{
-    const response = await fetch("https://bh5xnfiovnf7cjmw4liy2qzaye0darsq.lambda-url.us-east-1.on.aws/");
+    const response = await fetch("https://evjwodhvurdul6u545smt3yfay0syutw.lambda-url.us-east-1.on.aws/");
     const data = await response.json();
     counter.innerHTML = `views:  ${data}`;
   } catch(error){
