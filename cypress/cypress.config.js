2cb287019597ba10b1f55ed4d33d920371fb04b0
zirui2333 <zhengzirui43@gmail.com>
2024-05-27 15:16:28 -0400

    package.json added 10

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
 
   e2e: {
     baseUrl: 'https://evjwodhvurdul6u545smt3yfay0syutw.lambda-url.us-east-1.on.aws/',
-    specPattern: "./cypress/python_test.cy.js"
+    specPattern: "./cypress/python_test.cy.js",
+    supportFile: false
   },
 });
