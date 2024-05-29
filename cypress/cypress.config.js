8f522d3603a1110bb89de8da596e64129e0d7cb7
zirui2333 <zhengzirui43@gmail.com>
2024-05-28 21:19:23 -0400

    add terraform autmoation to github action 21

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
 
   e2e: {
-    baseUrl: 'https://evjwodhvurdul6u545smt3yfay0syutw.lambda-url.us-east-1.o.aws/',
+    baseUrl: 'https://evjwodhvurdul6u545smt3yfay0syutw.lambda-url.us-east-1.on.aws/',
     specPattern: "python_test.cy.js",
     supportFile: false
   },
