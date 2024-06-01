50e37dd4566506a15f3e108a508e334d86495764
zirui2333 <zhengzirui43@gmail.com>
2024-05-31 21:19:54 -0400

    Ajust HTML file

--- Actual_Resume_Web/Javascript-Counter/visitor.js
+++ Actual_Resume_Web/Javascript-Counter/visitor.js
@@ -2,13 +2,12 @@ const counter = document.querySelector(".counter_views")
 
 async function update_views(){
   try{
-    const response = await fetch("https://evjwodhvurdul6u545smt3yfay0syutw.lambda-url.us-east-1.on.aws/");
+    const response = await fetch("https://7m6lwapwwvhhjwvhnxwntpi2ba0nctzb.lambda-url.us-east-1.on.aws/");
     const data = await response.json();
     const body = JSON.parse(data.body)
-    counter.innerHTML = `views:  ${body.count}`;
+    counter.innerHTML = `${body.count}`;
   } catch(error){
-    counter.innerHTML = error;
-    console.log(`fail`)
+    counter.innerHTML = `43`
   }
 }
 
