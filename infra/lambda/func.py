89f7642f42aea803d7a1ad93f1a973d426fbd604
zirui2333 <zhengzirui43@gmail.com>
2024-05-31 21:51:54 -0400

    change py code

--- infra/lambda/func.py
+++ infra/lambda/func.py
@@ -23,11 +23,6 @@ def lambda_handler(event, context):
         })
     return_format = {
             'statusCode': 200,
-            'headers': {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Methods': 'GET'
-            },
             'body': json.dumps({
                 'message': 'success',
                 'count': str(views)
