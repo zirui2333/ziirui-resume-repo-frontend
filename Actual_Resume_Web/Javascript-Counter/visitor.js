bdedf41a4a55b993241b483797596359cb04aa9d
zirui2333 <zhengzirui43@gmail.com>
2024-05-31 21:26:44 -0400

    frontend modify

--- Actual_Resume_Web/Javascript-Counter/visitor.js
+++ Actual_Resume_Web/Javascript-Counter/visitor.js
@@ -4,8 +4,8 @@ async function update_views(){
   try{
     const response = await fetch("https://7m6lwapwwvhhjwvhnxwntpi2ba0nctzb.lambda-url.us-east-1.on.aws/");
     const data = await response.json();
-    const body = JSON.parse(data.body)
-    counter.innerHTML = `${body.count}`;
+    const body_data = JSON.parse(data.body)
+    counter.innerHTML = `${body_data.count}`;
   } catch(error){
     counter.innerHTML = `43`
   }
