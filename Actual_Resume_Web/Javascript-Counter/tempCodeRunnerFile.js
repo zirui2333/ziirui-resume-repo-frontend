f51fe4c30d0ad8d3485c7059b3faa40fc06e6193
Zirui Zheng <92755042+zirui2333@users.noreply.github.com>
2024-08-20 12:30:53 -0400

    Delete Actual_Resume_Web directory

--- Actual_Resume_Web/Javascript-Counter/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-socket.addEventListener("open", (event) => {
-  //   socket.send("Hello Server!");
-  // });
-
-  // // Listen for messages
-  // socket.addEventListener("message", (event) => {
-  //   console.log("Message from server ", event.data);
-  // });
